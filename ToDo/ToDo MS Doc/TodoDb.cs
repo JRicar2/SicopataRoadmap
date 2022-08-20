0a984a9920f7413ea68da8b9125da5d6810d2bd0
Juan Ricardo <79026814+JRicar2@users.noreply.github.com>
2022-08-20 13:02:25 -0400

    Agregado ToDo
    ToDo hecho con la siguiente documentación: https://docs.microsoft.com/en-us/aspnet/core/tutorials/min-web-api

---
+++ ToDo/ToDo MS Doc/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ToDo_MS_Doc
+{
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
